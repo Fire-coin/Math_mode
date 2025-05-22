bc2d21f66c280a62e0c14059aaab9d0cac2cec4f
Fire Coin <maxim.m.kollar@gmail.com>
2025-05-22 18:04:52 +0200

    Texture for ResearchTable fixed when viewed as an item.

---
+++ src/main/java/first/mathmode/item/EnergyRod.java
@@ -0,0 +1,42 @@
+package first.mathmode.item;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.WaterFluid;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class EnergyRod extends Item {
+    public EnergyRod(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        // Only do this on the server
+        if (!world.isClient) {
+            // Check what the player is looking at
+            HitResult hitResult = user.raycast(5.0D, 0.0F, false);
+            if (hitResult.getType() == HitResult.Type.BLOCK) {
+                BlockHitResult blockHit = (BlockHitResult) hitResult;
+                // Get the position of the block being looked at
+                var pos = blockHit.getBlockPos();
+
+                // Replace that block with water
+                world.setBlockState(pos, Blocks.WATER.getDefaultState());
+
+                // Optional: message the player
+                user.sendMessage(Text.literal("Transformed into water!"), true);
+            }
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
--- src/main/java/first/mathmode/item/ModItems.java
+++ src/main/java/first/mathmode/item/ModItems.java
@@ -2,8 +2,10 @@ package first.mathmode.item;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
@@ -13,6 +15,7 @@ public final class ModItems {
     }
 
     public static final Item EQUATION = register("equation", Equation::new, new Item.Settings());
+    public static final Item ENERGY_ROD = register("energy_rod", EnergyRod::new, new Item.Settings());
 
     public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("mathmode", path));
