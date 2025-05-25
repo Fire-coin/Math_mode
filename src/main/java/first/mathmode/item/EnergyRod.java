dad500b71036a9fcc197586cff29ed2c03ba8d1d
Fire Coin <maxim.m.kollar@gmail.com>
2025-05-25 10:24:52 +0200

    Added easter egg into EnergyRod and now it cannot destroy container blocks and crafting blocks, portals, end portal frame and dragon egg. Added small giude to research table.

--- src/main/java/first/mathmode/item/EnergyRod.java
+++ src/main/java/first/mathmode/item/EnergyRod.java
@@ -1,12 +1,15 @@
 package first.mathmode.item;
 
-import net.minecraft.block.Blocks;
+import first.mathmode.block.ResearchTable;
+import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -25,6 +28,27 @@ public class EnergyRod extends Item {
             HitResult hitResult = user.raycast(5.0D, 0.0F, false);
             if (hitResult.getType() == HitResult.Type.BLOCK) {
                 BlockHitResult blockHit = (BlockHitResult) hitResult;
+                BlockState blockState = world.getBlockState(blockHit.getBlockPos());
+                Block block = blockState.getBlock();
+                if (block instanceof ChestBlock ||
+                    block instanceof EnderChestBlock ||
+                    block instanceof SignBlock ||
+                    block instanceof AbstractFurnaceBlock ||
+                    block instanceof ShulkerBoxBlock ||
+                    block instanceof BeaconBlock ||
+                    block instanceof EndPortalFrameBlock ||
+                    block instanceof Portal ||
+                    block instanceof ResearchTable ||
+                    block instanceof EnchantingTableBlock ||
+                    block instanceof CraftingTableBlock ||
+                    block instanceof SmithingTableBlock ||
+                    block instanceof AnvilBlock ||
+                    block instanceof BrewingStandBlock ||
+                    block instanceof BarrelBlock ||
+                    block instanceof DragonEggBlock ||
+                    block instanceof RespawnAnchorBlock) {
+                    return ActionResult.PASS;
+                }
                 // Get the position of the block being looked at
                 var pos = blockHit.getBlockPos();
 
@@ -42,5 +66,13 @@ public class EnergyRod extends Item {
         if (world.getRandom().nextInt(1000) == 0) {
             target.kill((ServerWorld) world);
         }
+    }
+
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if (entity instanceof SheepEntity) {
+            ((SheepEntity) entity).setColor(DyeColor.YELLOW);
+        }
+        return ActionResult.SUCCESS;
     }
 }
